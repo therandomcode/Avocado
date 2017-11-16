c255585da125dae6590ca93349db6c079004f509
Kris Wagner <kriswagner.ch@gmail.com>
2017-11-15 21:01:37 -0500

    added screens

--- app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupSetPickupLocation.java
+++ app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupSetPickupLocation.java
@@ -1,7 +1,10 @@
 package com.example.wagner.avocado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FarmerRequestPickupSetPickupLocation extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class FarmerRequestPickupSetPickupLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_request_pickup_set_pickup_location);
+
+        final Button button = findViewById(R.id.farmerRequestPickupSetPickupLocationNextButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent farmerRequestPickupSetPickupLocationIntent = new Intent(FarmerRequestPickupSetPickupLocation.this,
+                        FarmerRequestPickupSetPickupLocationType.class);
+                startActivity(farmerRequestPickupSetPickupLocationIntent);
+            }
+        });
     }
 }
