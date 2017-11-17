491f331ef103ef8e236c8ddd8e3b22d76f4c5438
Kris Wagner <kriswagner.ch@gmail.com>
2017-11-17 17:23:12 -0500

    small bug fixes

--- app/src/main/java/com/example/wagner/avocado/SignUpSetLocation.java
+++ app/src/main/java/com/example/wagner/avocado/SignUpSetLocation.java
@@ -1,7 +1,10 @@
 package com.example.wagner.avocado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpSetLocation extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SignUpSetLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_set_location);
+
+        final Button signOutButton = findViewById(R.id.signUpSetLocationNextButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SignUpSetLocation.this, SignUpFarmerAddPhotos.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
