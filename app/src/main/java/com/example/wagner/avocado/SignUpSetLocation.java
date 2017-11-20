202828307ed7191ded8f2574d3853d9e7bb9923b
Kylee Tiger Santos <kyleetigersantos@gmail.com>
2017-11-20 13:10:28 -0500

    added maps to sign up screen
    made the enter address and drop pin options a toggle

--- app/src/main/java/com/example/wagner/avocado/SignUpSetLocation.java
+++ app/src/main/java/com/example/wagner/avocado/SignUpSetLocation.java
@@ -1,22 +1,64 @@
 package com.example.wagner.avocado;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton;
 
-public class SignUpSetLocation extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SignUpSetLocation extends AppCompatActivity implements
+        OnMarkerClickListener,
+        OnMapClickListener,
+        OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        OnMapAndViewReadyListener.OnGlobalLayoutAndMapReadyListener {
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
+
+    private GoogleMap mMap = null;
+
+    /**
+     * Keeps track of the selected marker.
+     */
+    private Marker mLastMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_set_location);
 
-        //findViewById(R.id.map).setVisibility(View.GONE);
+        findViewById(R.id.map).setVisibility(View.GONE);
 
         final Button signOutButton = findViewById(R.id.signUpSetLocationNextButton);
         signOutButton.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +67,11 @@ public class SignUpSetLocation extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        new OnMapAndViewReadyListener(mapFragment, this);
+
 
         final ToggleButton enterAddressButton = findViewById(R.id.signUpSetLocationEnterAddressButton);
         final ToggleButton dropPinButton = findViewById(R.id.signUpSetLocationDropPinButton);
@@ -68,7 +115,7 @@ public class SignUpSetLocation extends AppCompatActivity {
         city.setVisibility(View.VISIBLE);
         country.setVisibility(View.VISIBLE);
         postalCode.setVisibility(View.VISIBLE);
-        //findViewById(R.id.map).setVisibility(View.INVISIBLE);
+        findViewById(R.id.map).setVisibility(View.GONE);
     }
 
     private void setDropPinTextView() {
@@ -82,6 +129,111 @@ public class SignUpSetLocation extends AppCompatActivity {
         city.setVisibility(View.GONE);
         country.setVisibility(View.GONE);
         postalCode.setVisibility(View.GONE);
-        //findViewById(R.id.map).setVisibility(View.VISIBLE);
+        findViewById(R.id.map).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+
+        // Set listener for marker click event.  See the bottom of this class for its behavior.
+        mMap.setOnMarkerClickListener(this);
+
+        // Set listener for map click event.  See the bottom of this class for its behavior.
+        mMap.setOnMapClickListener(this);
+
+        // Override the default content description on the view, for accessibility mode.
+        // Ideally this string would be localized.
+        map.setContentDescription("Agromovil Map Test.");
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(4.5709,-74.2973), (float) 5.0));
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "Going to your location", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    @Override
+    public void onMapClick(final LatLng point) {
+        // Any showing info window closes when the map is clicked.
+        // Clear the currently selected marker.
+        if (mLastMarker != null) mLastMarker.remove();
+        mLastMarker = mMap.addMarker(new MarkerOptions().position(point));
+        Toast.makeText(this, "Setting your pickup location", Toast.LENGTH_SHORT).show();
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        // The user has re-tapped on the marker which was already showing an info window.
+        if (marker.equals(mLastMarker)) {
+            mLastMarker.remove();
+            return true;
+        }
+
+        mLastMarker = marker;
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur.
+        return false;
     }
 }
