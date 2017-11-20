8b2bed3bbb26193b60b60cad4690ea91cd266d90
Kris Wagner <kriswagner.ch@gmail.com>
2017-11-20 11:58:49 -0500

    added missing screens
    Screens do not pull from the database - they just appear. It's essentially a click-through prototpye.

--- app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupSetDropoffLocation.java
+++ app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupSetDropoffLocation.java
@@ -1,7 +1,10 @@
 package com.example.wagner.avocado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FarmerRequestPickupSetDropoffLocation extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class FarmerRequestPickupSetDropoffLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_request_pickup_set_dropoff_location);
+
+        final Button button = findViewById(R.id.farmerRequestPickupSetDropoffLocationNextButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent farmerBeginRequestPickupIntent = new Intent(FarmerRequestPickupSetDropoffLocation.this,
+                        FarmerRequestPickupChooseTransporter.class);
+                startActivity(farmerBeginRequestPickupIntent);
+            }
+        });
     }
 }
