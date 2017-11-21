0c9340e22aa65638fecefcc9fbdb43f6d30b272e
Kylee Tiger Santos <kyleetigersantos@gmail.com>
2017-11-21 00:24:53 -0500

    added Joyce's transporters list

--- app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupChooseTransporter.java
+++ app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupChooseTransporter.java
@@ -5,13 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class FarmerRequestPickupChooseTransporter extends AppCompatActivity {
+
+    ListView lst;
+    String[] transportername={"John","Richard","David"};
+    String[] time={"1996 Toyota Tacoma","2002 Nissan Navara","2000 Agrale Marrua"};
+    Integer[] imgid ={R.drawable.bgavocado,R.drawable.bgavocado,R.drawable.bgavocado};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_request_pickup_choose_transporter);
+
+        lst= findViewById(R.id.listview);
+        FarmerRequestPickupChooseTransporterCustomListView customListview = new FarmerRequestPickupChooseTransporterCustomListView(this,transportername,time,imgid);
+        lst.setAdapter(customListview);
 
         final Button button = findViewById(R.id.farmerRequestPickupChooseTransporterNextButton);
         button.setOnClickListener(new View.OnClickListener() {
