77b68dd8cfc674553510d45c1e037f45c9029ed9
Jeremy Xue <jeremyxue@gmail.com>
2017-11-29 03:58:23 -0500

    Messages
    Messages Added

---
+++ app/src/main/java/com/example/wagner/avocado/Messages.java
@@ -0,0 +1,17 @@
+package com.example.wagner.avocado;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jeremy on 11/29/17.
+ */
+
+public class Messages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_messages);
+    }
+}
