20031faec9583fa5975e1c2db793b374c39842b3
Kris Wagner <kriswagner.ch@gmail.com>
2017-11-29 11:29:59 -0500

    Credit card screens

--- app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupEnterPayment.java
+++ app/src/main/java/com/example/wagner/avocado/FarmerRequestPickupEnterPayment.java
@@ -12,12 +12,46 @@ public class FarmerRequestPickupEnterPayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_request_pickup_enter_payment);
+/*
+        final Button addCardButton = findViewById(R.id.farmerRequestPickupEnterAnotherPaymentAddCardButton);
+        addCardButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent farmerBeginRequestPickupIntent = new Intent(FarmerRequestPickupEnterPayment.this,
+                        FarmerRequestPickupEnterAnotherPayment.class);
+                startActivity(farmerBeginRequestPickupIntent);
+            }
+        });
 
         final Button button = findViewById(R.id.farmerRequestPickupEnterPaymentNextButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent farmerBeginRequestPickupIntent = new Intent(FarmerRequestPickupEnterPayment.this,
                         FarmerRequestPickupOrderConfirmation.class);
+                startActivity(farmerBeginRequestPickupIntent);
+            }
+        });
+
+        final Button addButton = findViewById(R.id.farmerRequestPickupEnterAnotherPaymentAddCardButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent farmerBeginRequestPickupAddCardIntent = new Intent(FarmerRequestPickupEnterPayment.this,
+                       FarmerRequestPickupEnterAnotherPayment.class);
+                startActivity(farmerBeginRequestPickupAddCardIntent);
+            }
+        }); */
+
+        final Button nextButton = findViewById(R.id.farmerRequestPickupEnterPaymentNextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent farmerBeginRequestPickupIntent = new Intent(FarmerRequestPickupEnterPayment.this, FarmerRequestPickupOrderConfirmation.class);
+                startActivity(farmerBeginRequestPickupIntent);
+            }
+        });
+
+        final Button addButton = findViewById(R.id.farmerRequestPickupEnterAnotherPaymentAddCardButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent farmerBeginRequestPickupIntent = new Intent(FarmerRequestPickupEnterPayment.this, FarmerRequestPickupEnterAnotherPayment.class);
                 startActivity(farmerBeginRequestPickupIntent);
             }
         });
