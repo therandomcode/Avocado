c333b940db17c3312c43496c89eb2288db18eebf
Kris Wagner <kriswagner.ch@gmail.com>
2017-12-07 02:17:08 -0500

    transporter messages

---
+++ app/src/main/java/com/example/wagner/avocado/TransporterMessagesListView.java
@@ -0,0 +1,66 @@
+package com.example.wagner.avocado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class TransporterMessagesListView extends ArrayAdapter<String> {
+    private String[] transportername;
+    private String[] time;
+    private Activity context;
+    private String[] price;
+    private Button[] acceptButton;
+    private Button[] rejectButton;
+
+    public TransporterMessagesListView(Activity context, String[] transportername, String[] time, String[] price) {
+        super(context, R.layout.activity_farmer_history_listview, transportername);
+        this.context = context;
+        this.transportername = transportername;
+        this.time = time;
+        this.price = price;
+        this.acceptButton = acceptButton;
+        this.rejectButton = rejectButton;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View r = convertView;
+        TransporterMessagesListView.ViewHolder viewHolder = null;
+        if (r == null) {
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r = layoutInflater.inflate(R.layout.activity_farmer_messages_list_view, null, true);
+            viewHolder = new TransporterMessagesListView.ViewHolder(r);
+            r.setTag(viewHolder);
+        } else {
+            viewHolder = (TransporterMessagesListView.ViewHolder) r.getTag();
+        }
+
+        viewHolder.tvw1.setText(transportername[position]);
+        viewHolder.tvw2.setText(time[position]);
+        viewHolder.tvw3.setText(price[position]);
+        return r;
+    }
+
+    class ViewHolder {
+        TextView tvw1;
+        TextView tvw2;
+        TextView tvw3;
+
+        ViewHolder(View v) {
+            tvw1 = v.findViewById(R.id.transportername);
+            tvw2 = v.findViewById(R.id.time);
+            tvw3 = v.findViewById(R.id.price);
+        }
+    }
+}
